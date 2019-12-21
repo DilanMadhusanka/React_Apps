d386dfd923a9a4e8cd126ca4e1b047d812874481
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2019-12-21 12:51:59 +0530

    add Spinner component and use renderContent function and learn defaultProps function

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner'
 
 class App extends React.Component {
 
@@ -13,7 +14,7 @@ class App extends React.Component {
         );
     }
 
-    render() {
+    renderContent() {
         if(this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
@@ -21,7 +22,13 @@ class App extends React.Component {
             return <SeasonDisplay lat= {this.state.lat} />
         }
 
-        return <div>Loading!</div>
+        return <Spinner message="Please accept the location request" />
+    }
+
+    render() {
+        return (
+            <div className="border red">{this.renderContent()}</div>
+        );
     }
 }
 
