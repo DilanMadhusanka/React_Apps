4e92c4b80c6268af19d7f6cce8e9e89fbc55edc4
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2019-12-27 23:40:43 +0530

    create SearchBar js file create Search bar

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import SearchBar from './SearchBar'
 
 class App extends React.Component {
     render() {
         return (
-            <div>App</div>
+            <div className="search-bar ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <lable>Video Search</lable>
+                        <input type="text" />
+                    </div>
+                </form>
+            </div>
         );
     }
 }
