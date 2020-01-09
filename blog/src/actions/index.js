d5c5dbbe434852af6e7e423651d08a175f743d6a
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-01-09 14:25:30 +0530

    one time memoization

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -6,14 +6,18 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-// export const fetchUser = (id) => async dispatch => {
-//     const response = await jsonPlaceholder.get(`/users/${id}`);
-//     dispatch({ type: 'FETCH_USER', payload: response.data});
-// }
-
-export const fetchUser = _.memoize(function(id) { 
-    return _.memoize(async function(dispatch) {
-        const response = await jsonPlaceholder.get(`/users/${id}`);
-        dispatch({ type: 'FETCH_USER', payload: response.data});
-    })
+export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data});
 });
+
+// or can user following code for memoization
+
+
+// export const fetchUser = _.memoize(function(id) { 
+//     return _.memoize(async function(dispatch) {
+//         const response = await jsonPlaceholder.get(`/users/${id}`);
+//         dispatch({ type: 'FETCH_USER', payload: response.data});
+//     })
+// });
