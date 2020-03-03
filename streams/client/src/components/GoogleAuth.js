761c0a697da656d028d17126b4247d1cc93dc420
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-03-03 12:56:46 +0530

    rendering authentication status in GoogleAuth component

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,18 +1,32 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+    state = { isSignedIn: null };
 
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '130363752374-1phbc44ii9qlis9gv26ok1a2ch8coai3.apps.googleusercontent.com',
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
             });
         });
     }
 
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return <div>I don't know if we are signed in</div>;
+        } else if (this.state.isSignedIn) {
+            return <div>I am Signed in!</div>;
+        } else {
+            return <div>I am not signed in</div>;
+        }
+    }
+
     render() {
-        return <div>Google Auth</div>;
+        return <div>{this.renderAuthButton()}</div>;
     }
 }
 
