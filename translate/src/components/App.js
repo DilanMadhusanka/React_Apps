96bc5b1d196ee644852d81b9a0bbb3ddddad815e
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-03-09 14:59:20 +0530

    create translate react project and write code for selecting language

---
+++ translate/src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    state = { language: 'english' }
+
+    onLanguageChange = language => {
+        this.setState({ language });
+    }
+
+    render() {
+        return (
+            <div className="ui container">
+                <div>
+                    Select language:
+                    <i className="flag us"  onClick={() => this.onLanguageChange('english')} />
+                    <i className="flag lk" onClick={() => this.onLanguageChange('sinhala')} />
+                </div>
+                { this.state.language }
+            </div>);
+    }
+}
+
+export default App;
