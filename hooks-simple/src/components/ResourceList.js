681ab93cbb2777e407aa9be77dff12a8258ae742
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-03-12 06:14:07 +0530

    actual code reuse

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = ({resource}) => {
-
+const useResources = (resource) => {
     const [resources, setResources] = useState([]);
 
     useEffect(() => {
@@ -11,9 +10,16 @@ const ResourceList = ({resource}) => {
     
             setResources(response.data);
         })(resource)
-    }, [resource])
+    }, [resource]);
 
-    return <div>{resources.map(record => <li key={record.id}>{record.title}</li>)}</div>
+    return resources;
+};
+
+const ResourceList = ({resource}) => {
+
+    const resources = useResources(resource);
+
+    return <ul>{resources.map(record => <li key={record.id}>{record.title}</li>)}</ul>;
 }
 
 export default ResourceList;
